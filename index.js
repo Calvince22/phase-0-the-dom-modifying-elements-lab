6225faa4fe34eab7b0bc958d9c8d7f993699535b
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-12-08 14:42:03 +0300

    adding elemnts to js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+document.querySelector("#main").remove()
+name = "Caleb Kiptoo"
+newHeader.textContent = `${name} is the champion` 
